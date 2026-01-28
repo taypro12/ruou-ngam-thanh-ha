c4ddec5106ef6ae9d584f756d3352ed83baa9df7
taypro12 <buingoctay1@gmail.com>
2026-01-28 22:15:24 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // GitHub Pages / static hosting
-
   plugins: [react()],
-
+  base: "/ruou-ngam-thanh-ha/",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-    minify: "terser",
-
     rollupOptions: {
-      input: "index.html",
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom", "react-router-dom"],
-          ui: ["lucide-react"]
-        }
-      }
-    }
+      input: {
+        main: "./index.html",
+      },
+    },
   },
-
-  server: {
-    port: 3000,
-    strictPort: true
-  }
 });
